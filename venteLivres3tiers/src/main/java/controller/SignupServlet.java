1a609854ff32acc698a2db8b82b000268f4f61e2
yesminemedhioub <medhioubyesmine@isimsf.u-sfax.tn>
2024-05-05 14:40:34 +0100

    connection problem

---
+++ venteLivres3tiers/src/main/java/controller/SignupServlet.java
@@ -0,0 +1,49 @@
+package controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.Client;
+
+import java.io.IOException;
+
+/**
+ * Servlet implementation class SignupServlet
+ */
+@WebServlet("/SignupServlet")
+public class SignupServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SignupServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String email = request.getParameter("mail");
+		String login = request.getParameter("nom");
+		String mdp1 = request.getParameter("mdp");
+		String mdp2 = request.getParameter("vmdp");
+		if(mdp1.equals(mdp2)) {
+			Client.ajoutClient(login,mdp1);
+			request.getRequestDispatcher("index.html").forward(request, response);
+		}
+	}
+
+}
